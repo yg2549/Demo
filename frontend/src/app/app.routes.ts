a646aa3423068fd4299fb23e980214883869b96e
nht251 <nht251@nyu.edu>
2024-11-12 22:52:54 +0200

    organize files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ConorFormComponent } from './conor-form/conor-form.component';
-import { StressFormComponent } from './stress-form/stress-form.component';
-import { LightupIntroComponent } from './lightup-intro/lightup-intro.component';
-import { IntroComponent } from './intro/intro.component';
-import { ConclusionComponent } from './conclusion/conclusion.component';
+import { ConorFormComponent } from './questionnaire/conor-form/conor-form.component';
+import { StressFormComponent } from './questionnaire/stress-form/stress-form.component';
+import { LightupIntroComponent } from './questionnaire/lightup-intro/lightup-intro.component';
+import { IntroComponent } from './questionnaire/intro/intro.component';
+import { ConclusionComponent } from './questionnaire/conclusion/conclusion.component';
 
 export const routes: Routes = [
     { path: '', component: IntroComponent},
